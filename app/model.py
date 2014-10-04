ea4221827686adc6f1695940797af78a4364370d
Richard CLark <richard@redspider.co.nz>
2014-10-05 12:37:32 +1300

    Lots of stuff

--- app/model.py
+++ app/model.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 from app import app
+from trex.support import quantum
 from mongoengine import *
 from trex.support.model import BaseDocument as Document, BaseAudit, BaseIdentity, BaseUser
 from trex.support.mongoengine import QuantumField, LowerCaseEmailField, QuantumDateField
@@ -15,3 +16,37 @@ class Identity(BaseIdentity):
 
 class User(BaseUser):
     pass
+
+class Table(Document):
+    """
+    Grid table of data
+    """
+    created = QuantumField(default=quantum.now)
+    creator = ReferenceField('User')
+    title = StringField()
+    description = StringField()
+    source = ReferenceField('Source') # This could be None if the source is another table or something
+    data = DynamicField()
+
+
+class Source(Document):
+    """
+    Source document, XLS or whatever
+    """
+    created = QuantumField(default=quantum.now)
+    creator = ReferenceField('User')
+    title = StringField()
+    source = StringField()
+    filename = StringField()
+    file = ReferenceField('TrexUpload', required=True)
+
+class Graph(Document):
+    """
+    Graph/Chart definition
+    """
+    created = QuantumField(default=quantum.now)
+    creator = ReferenceField('User')
+    title = StringField()
+    description = StringField()
+    table = ReferenceField('Table')
+    # x and y axis selection?
--- app/view/index.py
+++ app/view/index.py
@@ -3,10 +3,11 @@
 from __future__ import absolute_import
 from app import app
 from app.support import auth
-from trex.flask import AuthBlueprint, render_html
+from trex.flask import AuthBlueprint, render_html, flash
 from trex.support import wtf, quantum
-from flask import abort, g, redirect, url_for
+from flask import abort, g, redirect, url_for, make_response
 import json
+import app.model as m
 
 
 blueprint = AuthBlueprint('index', __name__)
@@ -31,8 +32,10 @@ def import_test():
 @render_html()
 def import_begin():
     class Form(wtf.Form):
-        json_file = wtf.FileField(
-            'JSON file',
+        title = wtf.StringField('Title')
+        source = wtf.StringField('Source')
+        data_file = wtf.FileField(
+            'File to import',
             [wtf.validators.Required()],
             allow_clear = False,
         )
@@ -40,10 +43,79 @@ def import_begin():
     form = Form()
 
     if form.validate_on_submit():
-        form.json_file.data.to_file('import-in-progress.json')
-        return redirect(url_for('index.import_review'))
+        m.Source(title=form.title.data,
+                 source=form.source.data,
+                 file=form.data_file.data,
+                 created=quantum.now('Pacific/Auckland'),
+                 creator=g.user,
+                 filename=form.data_file.data.file.filename).save()
+        return redirect(url_for('index.sources'))
 
     return dict(form=form)
+
+@blueprint.route('/sources', auth=auth.public)
+@render_html()
+def sources():
+    return dict(sources=m.Source.objects().order_by('title'))
+
+@blueprint.route('/sources/<source_id>/download', auth=auth.public)
+def download_source(source_id):
+    source = m.Source.objects.get(id=source_id)
+    response = make_response(source.file.file.read())
+    response.headers["Content-Disposition"] = "attachment; filename=%s" % source.filename
+    return response
+
+@blueprint.route('/tables', auth=auth.public)
+@render_html()
+def tables():
+    return dict(table=m.Table.objects().order_by('title'))
+
+
+@blueprint.route('/tables/<table_id>/download', auth=auth.public)
+def download_table(source_id):
+    table = m.Table.objects.get(id=source_id)
+    # SOMETHING GOES HERE TO MAKE THINGS GO
+    csv_output = "OH GOD, NOT IMPLEMENTED, RUN ZOMBIES"
+    response = make_response(csv_output)
+    response.headers["Content-Disposition"] = "attachment; filename=table.csv"
+    return response
+
+@blueprint.route('/source/<source_id>/delete', auth=auth.public, methods=['POST'])
+def delete_source(source_id):
+    source = m.Source.objects.get(id=source_id)
+    if m.Table.objects(source=source).count() > 0:
+        flash('Source has existing tables, delete tables first')
+        return redirect(url_for('index.sources'), 302)
+    source.delete()
+    flash('Source deleted')
+    return redirect(url_for('index.sources'), 302)
+
+@blueprint.route('/tables/<table_id>/delete', auth=auth.public, methods=['POST'])
+def delete_table(table_id):
+    table = m.Table.objects.get(id=table_id)
+    if m.Graph.objects(table=table).count() > 0:
+        flash('Table has existing graphs, delete graphs first')
+        return redirect(url_for('index.tables'), 302)
+    table.delete()
+    flash('Table deleted')
+    return redirect(url_for('index.tables'), 302)
+
+@blueprint.route('/graphs', auth=auth.public)
+@render_html()
+def graphs():
+    return dict(table=m.Graph.objects().order_by('title'))
+
+@blueprint.route('/graph/<graph_id>/delete', auth=auth.public, methods=['POST'])
+def delete_graph(graph_id):
+    graph = m.graph.objects.get(id=graph_id)
+    graph.delete()
+    flash('Graph deleted')
+    return redirect(url_for('index.graphs'), 302)
+
+@blueprint.route('/table/<table_id>/charter', auth=auth.public, methods=['GET', 'POST'])
+@render_html()
+def charter():
+    return {}
 
 @blueprint.route('/import-review', auth=auth.public, methods=['GET', 'POST'])
 @render_html()
