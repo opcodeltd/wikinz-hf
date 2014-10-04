6b98ddaa9cb352c40b15acd0631b2590b94d2a04
Nigel McNie <nigel@mcnie.name>
2014-10-04 16:26:01 +1300

    Hacky connection to postgres

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,11 @@ from trex.flask import Flask
 
 app = Flask(__name__)
 
+from flask.ext.sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = app.settings.get('postgresql', 'url')
+app.psql = SQLAlchemy(app)
+app.query = app.psql.engine.execute
+
 from . import view
 import trex.support.blueprint_auth
 import trex.support.blueprint_user_management
